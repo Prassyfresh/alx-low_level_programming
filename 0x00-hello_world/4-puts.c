32049452e0704cfc86b70f7c5bc23d40aabe834c
Prassyfresh <praiseonojs@gmail.com>
2022-12-08 22:56:01 -0800

    4th

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point 
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main()
+{
+	//string intialisation 
+	char Mystr[]= "Programming is like buliding a multilingual puzzle";
+
+	puts(Mystr); //writing the string to stdout
+
+	return 0;
+}
+
